45afb06e5a27b8a2e1a10bde67ab9260f12f5b11
Christopher Lewis <christopherlewis@Christophers-MacBook-Pro-2.local>
2017-08-21 16:54:18 -0400

    modified index.js so npm start would run

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,9 @@
-//const express = require('express')
+// require('babel-core/register')({
+// 	// "presets": ["es2015-node"]
+// 	// ignore false;
+// }); // hook for babel
+
+// const express = require('express')
 import express from 'express';
 const app = express();
 
@@ -12,6 +17,12 @@ app.use((err, req, res) => {
 	res.status(500).send(err);
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(8080, function() {
+	const host = this.address().address;
+	const port = this.address().port;
+	console.log(`Shard listening at http://${host}:${port}`);
+});
+
+// output all uncaught exceptions
+process.on('uncaughtException', err => console.exception('uncaught exception:', err));
+process.on('unhandledRejection', error => console.error('unhandled rejection:', err)); 
