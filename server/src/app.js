c8c659365370c2e8f916c6bbee40148c529a72fb
Christopher Lewis <christopherlewis@Christophers-MacBook-Pro-2.local>
2017-08-21 22:05:06 -0400

    modified app.js for morgan and winston

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,24 @@
+// npm packages
 import express from 'express';
+import bodyParser from "body-parser";
+import morgan from 'morgan';
+
+//  our packages
+import {logger} from './util/logger.js';
+
+// init app
 const app = express();
 
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}));
+
+
+// add body-parsing
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www--form-urlencoded
+
+
+// test method
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
