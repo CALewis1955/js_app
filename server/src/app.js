5d1a4eec56c148a0aeed5f250ee8a00faa3abcc8
Christopher Lewis <christopherlewis@Christophers-MacBook-Pro-2.local>
2017-08-25 16:15:35 -0400

    modifications for lesson 3

--- server/src/app.js
+++ server/src/app.js
@@ -23,6 +23,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+app.post('/login', (req, res) => {
+	const {username, password} = req.body;
+	if (username === 'test' && password === '123') {
+		res.send({username: 'test',	id: 1});
+		return;
+	}
+	res.status(401).send(error, 'Incorrect login');
+});
+
 // catch all unhandled errors
 app.use((err, req, res) => {
 	console.error(err.stack);
